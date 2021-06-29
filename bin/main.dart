09dedfa9575c6d30e58c867f63601e820a058275
Rahul Badgujar <44890430+imRB7here@users.noreply.github.com>
2021-06-29 17:44:02 +0530

    welcome rahul

--- bin/main.dart
+++ bin/main.dart
@@ -17,7 +17,7 @@ Future<void> configHive() async {
 Future<void> configServer() async {
   final app = Alfred();
 
-  app.all('/example', (req, res) => 'Hello world');
+  app.all('/example', (req, res) => 'Hello rahul');
 
   app.get('/uidForEmail/:email', (req, res) {
     final email = req.params['email'];
